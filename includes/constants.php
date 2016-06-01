fc32c7204e5d09e24b070a53c884be9be0a6143a
anshulgarg19 <garg_anshul94@ymail.com>
2016-06-02 00:33:59 +0530

    Add files via upload

---
+++ includes/constants.php
@@ -0,0 +1,23 @@
+<?php
+
+    /***********************************************************************
+     * constants.php
+     *
+     * AppSpace by Anshul Garg
+     *
+     * Global constants.
+     **********************************************************************/
+
+    // your database's name
+    define("DATABASE", "*****");
+
+    // your database's password
+    define("PASSWORD", "*****");
+
+    // your database's server
+    define("SERVER", "localhost");
+
+    // your database's username
+    define("USERNAME", "root");
+
+?>
